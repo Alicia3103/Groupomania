f663c17d2fb8c1fd32e0f8ac84a08b5e557ef760
Alicia3103 <charlot.alicia3103@gmail.com>
2022-06-30 17:05:15 +0200

    ajout style, accueil, nav et post container

--- front/src/components/Navigation/Navigation.js
+++ front/src/components/Navigation/Navigation.js
@@ -1,22 +1,56 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { faUser ,faRightFromBracket,faHouse} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon}from '@fortawesome/react-fontawesome'
+import styled from 'styled-components';
+import colors from '../../utils/styles/colors';
+
+const NavComponent=styled.div`
+width:23%;
+display:flex;
+justify-content: flex-start;
+align-self: start;
+}
+
+`
+const NavList=styled.ul`
+list-style : none;
+display:flex;
+flex-direction:column;
+padding:0;
+margin:0
+`
+const NavIcon=styled.li`
+display:flex;
+justify-content:center;
+align-items:center;
+text-decoration: none;
+font-size:20px;
+background-color:${colors.secondary};
+color: ${colors.darkerSecondary};
+border-radius:10px;
+margin:10px;
+height:40px;
+width:40px;
+
+`
 
 const Navigation = () => {
     return (
-        <div className='navigation'>
-            <ul>
+        <NavComponent>
+            <NavList>
                 <NavLink to="/accueil">
-                    <li>Accueil</li>
+                <NavIcon><FontAwesomeIcon icon={faHouse}></FontAwesomeIcon></NavIcon>
                 </NavLink>
                 <NavLink to="/user">
-                    <li>Page perso</li>
+                    <NavIcon><FontAwesomeIcon icon={faUser}></FontAwesomeIcon></NavIcon>
                 </NavLink>
                 <NavLink to="">
-                    <li>Deconnexion</li>
+                <NavIcon><FontAwesomeIcon icon={faRightFromBracket}></FontAwesomeIcon></NavIcon>
                 </NavLink>
-            </ul>
+            </NavList>
             
-        </div>
+        </NavComponent>
     );
 };
 
--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -7,26 +7,52 @@ import { isEmpty } from '../utils/Utils';
 import styled from 'styled-components'
 import backgroundImage from './groupomaniafond.jpg'
 import PostForm from '../components/PostForm/PostForm';
+import colors from '../utils/styles/colors';
 
 const HomePage=styled.div`
-`
-
-const HomePageContainer=styled.div`
 display:flex;
-align-items: center;
-justify-content: center;
-flex-grow:3;
+flex-direction:column;
 height:100vh;
-width:100vw;
 background-image: url(${backgroundImage});
 background-position: center; 
 background-repeat: no-repeat; 
 background-size: cover;
 `
-const PostsContainer= styled.section``
 
-const NewPostContainer=styled.div``
-const OldPostsContainer=styled.div``
+const HomePageContainer=styled.div`
+display:flex;
+align-items: center;
+justify-content: flex-start;
+width:100vw;
+
+
+`
+const PostsContainer= styled.div`
+display:flex;
+width: 50%;
+flex-direction: column;
+justify-content: space-around;
+align-items: center;
+
+`
+
+const NewPostContainer=styled.div`
+
+height:300px;
+width:100%;
+background-color:${colors.secondary};
+border-radius:10px;
+
+`
+const OldPostsContainer=styled.div`
+display:flex;
+flex-direction: column;
+justify-content: space-around;
+align-items: center;
+width:100%;
+background-color:${colors.secondary};
+border-radius:10px;
+`
 
 const Home = () => {
 const posts= useSelector((state)=>state.postReducer)
